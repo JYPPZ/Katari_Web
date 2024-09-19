e9779144e2e2eb7ebc960efe68f13183e08cb8a0
JarbyDaniel <dsalazarg@unicauca.edu.co>
2024-09-19 04:20:38 -0500

    interface base

--- servidor/app.js
+++ servidor/app.js
@@ -5,6 +5,9 @@ const app = express();
 // Servir archivos estáticos de la carpeta cliente
 app.use(express.static(join(__dirname, '../cliente')));
 
+app.use(express.static(path.join(__dirname, '../public')));
+
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
